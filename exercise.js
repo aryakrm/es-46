f8a02edf85653edd175e0d0716ed159115f94fb4
Arya Karimi <devaryakrm@gmal.com>
2022-09-18 22:15:40 +0300

    codes added

---
+++ exercise.js
@@ -0,0 +1,12 @@
+const student = {
+  id: 1,
+  school: "Liceo",
+  year: 3,
+  personalData: {
+    name: "Arya",
+    surname: "Karimi",
+    age: 23,
+  },
+};
+
+console.log(student.personalData);
